ca115224465bafea825b2d93c93b0410c3fd2a42
Infanymous <infanymousss@gmail.com>
2018-12-15 00:56:05 +0100

    gitignore fix

--- BinaryDecisionDiagram/Program.cs
+++ BinaryDecisionDiagram/Program.cs
@@ -16,9 +16,12 @@ namespace BinaryDecisionDiagram
             g.Generate(); //generuje iloczyn kartezjanski przeslanek i randomowe wartosci konkluzji
             g.FindQuantities(); //znajduje ile jest kolumn z kazda konkluzja
 
-            //g.PrintTab(150);
+            
+            g.PrintTab(150);
 
             double naszeI = (g.Entropy(g.n1, g.n) + g.Entropy(g.n1, g.n) + g.Entropy(g.n1, g.n) + g.Entropy(g.n1, g.n));
+            g.I = naszeI;
+            g.GetEntropyForRow(0);
             Console.WriteLine(g.Entropy(g.n1, g.n) + "    = Entropia, dla " + g.n1 + " konkluzji nr 1 (szlak czerwony)" );
             Console.WriteLine(g.Entropy(g.n2, g.n) + "    = Entropia, dla " + g.n2 + " konkluzji nr 2 (szlak niebieski)");
             Console.WriteLine(g.Entropy(g.n3, g.n) + "    = Entropia, dla " + g.n3 + " konkluzji nr 3 (szlak zielony)");
@@ -28,7 +31,7 @@ namespace BinaryDecisionDiagram
             Console.WriteLine();
 
             int desiredRow = 0;
-            int columns = g.CountWantedColumns(desiredRow);
+            int columns = g.CountWantedColumns(desiredRow, true);
             Console.WriteLine(columns + " tyle mamy kolumn z potwierdzonym warunkiem 1 (wiersz 1 tablicy), (dokladnie polowa, bo to warunek tak/nie a mamy iloczyn kartezjanski)");
             int nplus=0, n1plus = 0, n2plus = 0, n3plus = 0, n4plus = 0;
             int nminus = 0, n1minus = 0, n2minus = 0, n3minus = 0, n4minus = 0;
@@ -57,15 +60,15 @@ namespace BinaryDecisionDiagram
             double entropyWarunek1plus = (g.Entropy(n1plus, nplus) + g.Entropy(n2plus, nplus) + g.Entropy(n3plus, nplus) + g.Entropy(n4plus, nplus)); //entropia dla potwierdzonego warunku 1
             double entropyWarunek1minus = (g.Entropy(n1minus, nminus) + g.Entropy(n2minus, nminus) + g.Entropy(n3minus, nminus) + g.Entropy(n4minus, nminus)); //entropia dla zaprzeczonego warunku 1
 
-            Console.WriteLine("ilosc kazdej konkluzji po potwierdzeniu warunku1 (na koncu laczna liczba potwierdzonych warunkow 1): " + n1plus + " " + n2plus + " " + n3plus + " " + n4plus + " " + nplus);
+            Console.WriteLine("ilosc kazdej konkluzji po potwierdzeniu warunku1 (na koncu laczna liczba potwierdzonych warunkow 1): " + n1plus  + " " + n2plus + " " + n3plus + " " + n4plus + " " + nplus);
             Console.WriteLine("ilosc kazdej konkluzji po zaprzeczeniu warunku1: " + n1minus + " " + n2minus + " " + n3minus + " " + n4minus + " " + nminus);
-            Console.WriteLine("entropia po potwierdzeniu, czy mamy towarzysza (warunek 1+): " + entropyWarunek1plus);
-            Console.WriteLine("entropia po zaprzeczeniu, czy mamy towarzysza (warunek 1-): " + entropyWarunek1minus);
+            Console.WriteLine("entropia po potwierdzeniu, czy mamy towarzysza (warunek 1+): " + entropyWarunek1plus + " " + g.GetEntropyForRow(0)[0]);
+            Console.WriteLine("entropia po zaprzeczeniu, czy mamy towarzysza (warunek 1-): " + entropyWarunek1minus + " " + g.GetEntropyForRow(0)[1]);
 
             double naszeE1 = (144.0 / 288.0 * entropyWarunek1minus + 144.0 / 288.0 * entropyWarunek1plus);
             Console.WriteLine("suma tych entropii * liczebnosc (nasze E1): " + naszeE1);//144/288 dlatego, ze w iloczynie kartezjanskim mamy od razu polowe warunku 1 w jedynkach, a polowe w zerach
 
-            Console.WriteLine("I - E1: " + (naszeI - naszeE1));
+            Console.WriteLine("I - E1: " + (naszeI - naszeE1) + " " + g.GetEntropyForRow(0)[2]);
         }
     }
 }
--- BinaryDecisionDiagram/Tabela.cs
+++ BinaryDecisionDiagram/Tabela.cs
@@ -8,9 +8,11 @@ namespace BinaryDecisionDiagram
 {
     class Tabela
     {
+        int columns = 288, rows = 20;
         public int[,] tabelka;
         public int n, n1, n2, n3, n4;
-
+        public double I;
+        
 
         public Tabela()
         {
@@ -19,7 +21,8 @@ namespace BinaryDecisionDiagram
 
         public Tabela(int cols, int [,] values)
         {
-            tabelka = new int[20, cols];
+            columns = cols;
+            tabelka = new int[rows, columns];
 
             if (values.Length != tabelka.Length)
             {
@@ -34,12 +37,12 @@ namespace BinaryDecisionDiagram
             int towarzysz = 2, pogoda = 3, kondycja = 3, temperatura = 4, wiek = 4; //rozmiar przeslanki (np. tak/nie -> 2)
             int iT = 0, iP = 0, iK = 0, iTe = 0, iW = 0; //iteratory potrzebne pozniej
             int size = towarzysz * pogoda * kondycja * temperatura * wiek; //wszystkie mozliwosci, 288
-            int[,] tab = new int[20, size]; //20 wierszy bo tyle przeslanek+konkluzji(konkluzje beda potem), 288 kolumn
-            int[] helper = new int[20];
+            int[,] tab = new int[rows, size]; //20 wierszy bo tyle przeslanek+konkluzji(konkluzje beda potem), 288 kolumn
+            int[] helper = new int[rows];
 
             for (int i = 0; i < size; i++) //lecimy po kolei po kazdej kolumnie
             {
-                helper = new int[20]; //na poczatku musimy wyzerowac uzywana tablice, bo sie zapelnia jedynkami po kazdym przejsciu
+                helper = new int[rows]; //na poczatku musimy wyzerowac uzywana tablice, bo sie zapelnia jedynkami po kazdym przejsciu
                 
                 if (iW == 4) //przeslanka wiek ma 4 opcje, co 4 mozliwosci zerujemy, ale przesuwamy zmienna, ktora jest "o jeden wyzej w hierarchii" zeby zbadac wszystkie mozliwosci 
                 {
@@ -70,13 +73,13 @@ namespace BinaryDecisionDiagram
                 helper[12 + iW] = 1;
                 iW++;
 
-                for (int j = 0; j < 20; j++)
+                for (int j = 0; j < rows; j++)
                 {
                     tab[j,i] = helper[j]; //wpisujemy helpera jako kolumne naszej tabelki
                 }
             }
 
-            tabelka = new int[20, 288];
+            tabelka = new int[rows, size];
             tabelka = tab;
             SetRandomConclusions();
         }
@@ -99,16 +102,30 @@ namespace BinaryDecisionDiagram
             //}
         }
 
-        public int CountWantedColumns(int desiredRow)
+        public int CountWantedColumns(int desiredRow, bool plusOrMinus)
         {
             int cols = 0;
-            for (int i = 0; i < tabelka.GetLength(1); i++)
+            if (plusOrMinus)
             {
-                if (tabelka[desiredRow, i]==1)
+                for (int i = 0; i < tabelka.GetLength(1); i++)
                 {
-                    cols++;
+                    if (tabelka[desiredRow, i] == 1)
+                    {
+                        cols++;
+                    }
                 }
             }
+            else
+            {
+                for (int i = 0; i < tabelka.GetLength(1); i++)
+                {
+                    if (tabelka[desiredRow, i] == 0)
+                    {
+                        cols++;
+                    }
+                }
+            }
+            
 
             return cols;
         }
@@ -157,6 +174,44 @@ namespace BinaryDecisionDiagram
             //Console.WriteLine();
             //Console.WriteLine();
             //Console.Write(n1 + " " + n2 + " " + n3 + " " + n4);
+        }
+
+        public double[] GetEntropyForRow(int row)
+        {
+            int countOnes = 0, countZeroes = 0, n1plus = 0, n2plus = 0, n3plus = 0, n4plus = 0, n1minus = 0, n2minus = 0, n3minus = 0, n4minus = 0;
+
+            for (int i = 0; i < tabelka.GetLength(1); i++)
+            {
+                if (tabelka[row, i] == 1)
+                {
+                    countOnes++;
+                    n1plus += tabelka[rows - 4, i];
+                    n2plus += tabelka[rows - 3, i];
+                    n3plus += tabelka[rows - 2, i];
+                    n4plus += tabelka[rows - 1, i]; //mamy info, dla ilu potwierdzen warunku1 mamy spelnionych przeslanek 1, 2, 3, 4
+                }
+                else
+                {
+                    countZeroes++;
+                    n1minus += tabelka[rows - 4, i];
+                    n2minus += tabelka[rows - 3, i];
+                    n3minus += tabelka[rows - 2, i];
+                    n4minus += tabelka[rows - 1, i];//mamy info, dla ilu zaprzeczen warunku1 mamy spelnionych kazdych przeslanek
+                }
+            }
+
+            double entropyPlus = Entropy(n1plus, countOnes) + Entropy(n2plus, countOnes) + Entropy(n3plus, countOnes) + Entropy(n4plus, countOnes);
+            double entropyMinus = Entropy(n1minus, countZeroes) + Entropy(n2minus, countZeroes) + Entropy(n3minus, countZeroes) + Entropy(n4minus, countZeroes);
+            double Ej = GetEj(entropyPlus, countOnes, entropyMinus, countZeroes);
+
+            double IminusEj = I - Ej;
+
+            return new double[] { entropyPlus, entropyMinus, IminusEj };
+        }
+
+        public double GetEj(double entropyPlus, int countOnes, double entropyMinus, int countZeroes)
+        {
+            return (((double)countOnes / (double)columns) * entropyPlus + ((double)countZeroes / (double)columns) * entropyMinus);
         }
 
         public double Entropy(int n1, int n)
